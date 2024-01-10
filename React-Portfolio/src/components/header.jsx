9b670a1f21e5b5532c1a1d034706dea5d3b244ef
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-10 15:17:22 -0400

    Set up the react basics and got a header component

---
+++ React-Portfolio/src/components/header.jsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css";
+
+function Header() {
+    return (
+        <header className="HeaderComponent">
+            <h1>Joshua's React Portfolio</h1>
+        </header>
+    );
+}
+
+export default Header;
